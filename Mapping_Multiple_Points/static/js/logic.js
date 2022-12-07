0dd029603e34c100bfec750f9075f7c9daa81f8d
shawnkitagawa <seankitagawa@gmail.com>
2022-12-06 22:35:44 -0800

    update the map

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,8 +4,28 @@ console.log("working");
 // create the map object with a center and zoom level.
 let map = L.map("mapid").setView([40.7,-94.5], 4);
 
+let cityData = cities;
+
+  //Loop throught the cities array and create one marker for each city.
+  cityData.forEach(function (city)
+  {
+    console.log(city);
+    L.circleMarker(city.location , 
+        {
+            radius: city.population/100000,
+            weight: 4,
+            fillColor: "#000000",
+            color: "orange"
+
+        })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "<h2> <hr> <h3>Population "
+    + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+  });
+
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
